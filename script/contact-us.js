57475ac91537b5cbb1927a920efc78feb18e5633
Lynn <koliilynn@gmail.com>
2025-02-13 10:08:29 +0300

    adding dom manipulation on my contact page

--- script/contact-us.js
+++ script/contact-us.js
@@ -1,6 +1,8 @@
 document
   .getElementById("contactForm")
   .addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevents form from refreshing the page
-    document.getElementById("successMessage").style.display = "block"; // Show success message
+    event.preventDefault(); 
+    document.getElementById("successMessage").style.display = "block"; 
   });
+
+  
